e859c9ab0a2c5305dfabcbe95150e51cd13796ac
Erxical <erxinxiao@gmail.com>
2022-03-22 23:55:51 +0700

    final touch with excited function

---
+++ Bootstrap/Assignment_2/js/js-file.js
@@ -0,0 +1,6 @@
+function getExcited(){
+    document.getElementById('getExciting').style.display="block";
+    var vid = document.getElementById('getExciting');
+    vid.play();
+    document.getElementById('getExciting').requestFullscreen();
+}
