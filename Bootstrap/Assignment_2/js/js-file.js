72f1f7282775d3f3c039af9bb15dc8e57ad7f06a
Erxical <erxinxiao@gmail.com>
2022-03-23 00:15:48 +0700

    wtf

--- Bootstrap/Assignment_2/js/js-file.js
+++
@@ -1,6 +0,0 @@
-function getExcited(){
-    document.getElementById('getExciting').style.display="block";
-    var vid = document.getElementById('getExciting');
-    vid.play();
-    document.getElementById('getExciting').requestFullscreen();
-}
